fce60997f6df28fb56b758ab3b9099fe76b3e1ea
tam11a <ibrahimsadiktamim@gmail.com>
2022-07-25 03:38:37 +0600

    added: task-1.c

--- assignment-1/task-1.c
+++ assignment-1/task-1.c
@@ -1,6 +1,54 @@
 #include <stdio.h>
+#include <math.h>
+
 int main()
 {
-    // code will be added here
+    // declaring variables
+    int arrLength = 8;  // array length, defined in question
+    int arr[arrLength]; // array of integer
+    int total = 0;      // total summation of arr
+
+    printf("Insert numbers: ");
+    int i = 0, j;
+    for (; i < arrLength; i++)
+    {
+        scanf("%d", &arr[i]);
+        total += arr[i];
+    }
+
+    // get value of mean
+    float mean = total / (float)arrLength;
+
+    int temp;
+    // sort the array
+    for (i = 0; i < arrLength; i++)
+    {
+        for (j = 0; j < arrLength; j++)
+        {
+            if (arr[j] > arr[j + 1])
+            {
+                // if next value is smaller that the value, swap!!
+                temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+
+    float median = 0;
+    // as the array length, 8 is even, median = (array[(n-1)/2] + array[n/2])/2.0;
+    median = (arr[(arrLength - 1) / 2] + arr[arrLength / 2]) / 2.0;
+
+    double sDeviation = 0;
+    // calculate sample standard deviation
+    for (i = 0; i < arrLength; i++)
+    {
+        sDeviation += pow(((double)arr[i]) - ((double)mean), 2);
+    }
+
+    // outputs
+    printf("The mean is : %.3f\n", mean);
+    printf("The median is : %g \n", median); // %g for remove trailing 0 from float type variable
+    printf("The standard deviation is : %.13lf\n", sqrt(sDeviation / (arrLength - 1)));
     return 0;
 }
